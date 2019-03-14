648132d463009e85290c8e4b2345d82a10540d6e
MattMoony <monschein.matthias@gmail.com>
2019-03-14 12:24:35 +0100

    correction

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ let options = {
 };
 let h4x0r = new H4x0r(target, options);
 
-h4x0r.display(' -- W3LC0M3 T0 H4X0R.J5 -- ', {center: true, heading: true,});
+h4x0r.display(' -- W3LC0M3 T0 1337 H4X0R.J5 -- ', {center: true, heading: true,});
 h4x0r.getInput("Ready for the &copy; Cisco Disco? [y/n] ", ans => {
   if (ans.toLowerCase() === 'y') {
     h4x0r.ciscoDisco();

b531174e0dc4f35b4a40718067d85098cc400a18
MattMoony <monschein.matthias@gmail.com>
2019-03-14 11:38:51 +0100

    little changes + updates

--- example/index.js
+++ example/index.js
@@ -1,11 +1,16 @@
 let target = document.getElementById('target');
 let options = {
+  showCreds: false,
 };
 let h4x0r = new H4x0r(target, options);
 
-h4x0r.display('Hello World!');
-h4x0r.getInput("What's 2+2?", ans => {
-  console.log(ans);
-  h4x0r.display(`Your answer was "${ans}" ... `);
-  h4x0r.close();
+h4x0r.display(' -- W3LC0M3 T0 H4X0R.J5 -- ', {center: true, heading: true,});
+h4x0r.getInput("Ready for the &copy; Cisco Disco? [y/n] ", ans => {
+  if (ans.toLowerCase() === 'y') {
+    h4x0r.ciscoDisco();
+    h4x0r.getInput("Press enter to stop ... ", () => h4x0r.close());
+  } else {
+    h4x0r.display('Ok, bye then!');
+    h4x0r.close();
+  }
 });
--- src/h4x0r.js
+++ src/h4x0r.js
@@ -4,14 +4,51 @@ class H4x0r {
     opts = opts || {};
 
     this.cursorSpeed = opts.cursorSpeed || 0.25;
-    this.showCreds = opts.showCreds || true;
+    this.showCreds = opts.showCreds !== undefined ? opts.showCreds : true;
+    this.inputNextLine = opts.inputNextLine || false;
+    this.anime = null;
 
     this.username = opts.username || "h4x0r";
     this.hostname = opts.hostname || "host";
     this.path = opts.path || "~";
     this.mode = opts.mode || "$";
 
+    this.fontColor = opts.fontColor || '#fff';
+    this.backgroundColor = opts.backgroundColor || '#151515';
+
     this.setup();
+  }
+
+  changeColor(c) {
+    this.target.style.color = c;
+  }
+
+  changeBackgroundColor(c) {
+    this.target.style.backgroundColor = c;
+  }
+
+  rainbowDisco() {
+    let color = 0;
+
+    this.anime = window.setInterval(() => {
+      this.target.style.color = `hsl(${color++%361}, 100%, 50%)`;
+      this.target.style.backgroundColor = `hsl(${361-((color-1)%361)}, 100%, 50%)`;
+    }, 10);
+  }
+
+  ciscoDisco() {
+    let randRGB = () => Math.floor(Math.random()*255);
+
+    this.anime = window.setInterval(() => {
+      this.target.style.color = `rgb(${randRGB()}, ${randRGB()}, ${randRGB()})`;
+      this.target.style.backgroundColor = `rgb(${randRGB()}, ${randRGB()}, ${randRGB()})`;
+    }, 75);
+  }
+
+  stopAnime() {
+    window.clearInterval(this.anime);
+
+    this.target.style = '';
   }
 
   setup() {
@@ -21,13 +58,14 @@ class H4x0r {
     let content = `
       .h4x0r-terminal {
         font-family: monospace;
-        background-color: #151515;
-        color: #fff;
+        background-color: ${this.backgroundColor};
+        color: ${this.fontColor};
         padding: 15px;
       }
 
       .h4x0r-line {
         user-select: none;
+        padding: 1.5px;
       }
       ${this.showCreds ? `.h4x0r-line::before {
         content: '${this.username}@${this.hostname}:${this.path}${this.mode} ';
@@ -53,7 +91,7 @@ class H4x0r {
     this.target.classList.add('h4x0r-terminal');
   }
 
-  display(str) {
+  display(str, opts) {
     let line = document.createElement('div'),
         output = document.createElement('div');
     line.classList.add('h4x0r-line');
@@ -61,6 +99,15 @@ class H4x0r {
 
     output.innerHTML = str;
     line.appendChild(output);
+
+    opts = opts || {};
+
+    if (opts.center)
+      line.style.textAlign = 'center';
+    if (opts.heading) {
+      line.style.fontSize = '1.25em';
+      line.style.fontWeight = 'bold';
+    }
 
     this.target.appendChild(line);
   }
@@ -77,11 +124,14 @@ class H4x0r {
 
     input.classList.add('h4x0r-input');
     input.contentEditable = true;
+    input.focus();
 
     line.appendChild(output);
     line.appendChild(input);
 
     line.onclick = () => input.focus();
+    this.target.onclick = line.onclick;
+
     input.onkeyup = e => {
       if (e.keyCode === 13) {
         input.contentEditable = false;
@@ -91,9 +141,11 @@ class H4x0r {
     };
 
     this.target.appendChild(line);
+    input.focus();
   }
 
   close() {
+    this.stopAnime();
     this.display('Press &lt;RETURN&gt; to close this console ... ');
   }
 }
